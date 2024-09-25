7dccda135523079144628a511b20103366006798
Luiz <162443980+Wheel23@users.noreply.github.com>
2024-09-25 07:44:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var numero = ''
+var operadorAtual = ''
+var numeroAnterior = ''
+function addNumero(caractere){
+    numero += caractere
+    document.getElementById('display').value = numero
+}
+function limparDisplay(){
+    numero = ''
+    operadorAtual = ''
+    numeroAnterior = ''
+    document.getElementById('display').value = ''
+}
+function apagar(){
+    let display = document.getElementById('display').value
+    document.getElementById('display').value = display.slice(0,-1)
+}
+function operador(op){
+    if(numero !== ''){
+        operadorAtual = op
+        numeroAnterior = numero
+        numero = ''
+        document.getElementById('display').value = ''
+    }
+}
+function calcular(){
+    let resultado = 0
+    let atual = parseFloat(numero)
+    let anterior = parseFloat(numeroAnterior)
+
+    if (operadorAtual === '+') {
+        resultado = anterior + atual
+    }else if (operadorAtual === '-'){
+        resultado = anterior - atual
+    }else if (operadorAtual === '*'){
+        resultado = anterior * atual
+    }else if(operadorAtual === '%'){
+        resultado = anterior / 100
+    }else if(operadorAtual === '√'){
+        resultado = Math.sqrt(anterior)
+    }else if(operadorAtual === '/'){
+        if(atual === 0){
+            document.getElementById('display').value = 'Erro: divisão por zero'
+            return
+        }
+        resultado = anterior / atual
+    }
+
+    document.getElementById('display').value = resultado
+    numero = resultado.toString()
+    operadorAtual = ''
+    numeroAnterior = ''
+}
